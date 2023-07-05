66ba81cfd6281d5cebade849dc58e2bc3042ebad
Leesa Ward <leesa@doubleedesign.com.au>
2023-07-05 20:55:32 +1000

    feat(GraphQL): Add type to front-end query

--- frontend/src/graphql/queries.ts
+++ frontend/src/graphql/queries.ts
@@ -1,6 +1,8 @@
 import gql from 'graphql-tag';
+import { TypedDocumentNode } from '@apollo/client';
+import { Subject } from '@server/types.ts';
 
-export const CURRENT_SUBJECTS_QUERY = gql`
+export const CURRENT_SUBJECTS_QUERY: TypedDocumentNode<Subject> = gql`
     query CurrentSubjectsQuery {
         getCurrentSubjects {
             unitCode,
