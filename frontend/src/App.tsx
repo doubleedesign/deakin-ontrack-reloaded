d48261d7973a12d8b305438d81dd846fa4e87e18
Leesa Ward <leesa@doubleedesign.com.au>
2023-07-07 22:46:53 +1000

    fix(GQL current subjects): Fix typing for front-end query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,23 +8,25 @@ import { Subject } from '@server/types.ts';
 
 function App() {
 	const { setCredentials, queryOptions } = useContext(AppContext);
-	const [getCurrentUnits] = useLazyQuery(CURRENT_SUBJECTS_QUERY, { fetchPolicy: 'no-cache' });
 	const [errors, setErrors] = useState<GraphQLError[]>([]);
-	const [loading, setLoading] = useState<boolean>(false);
+	const [getCurrentSubjects] = useLazyQuery(CURRENT_SUBJECTS_QUERY, { fetchPolicy: 'no-cache' });
 	const [currentSubjects, setCurrentSubjects] = useState<Subject[]>();
 
-	const handleOntrack = useCallback(async function handleOntrack() {
-		// Stop lying to me, TypeScript. The response contains a detailed 'errors' array.
+	function handleSubjects() {
+		// Stop lying to me, TypeScript. The response contains a detailed 'errors' array when there's errors.
 		// @ts-ignore
-		const { loading, data, errors } = await getCurrentUnits(queryOptions);
-		setLoading(loading);
+		const { data, errors } = await getCurrentSubjects(queryOptions);
 		if(errors)  {
 			setErrors(errors);
 		}
 		if(data) {
-			setCurrentSubjects(data.getCurrentSubjects);
+			setCurrentSubjects(data.subjects);
 		}
-	}, [getCurrentUnits, queryOptions]);
+	}
+
+	const handleOntrack = useCallback(async function handleOntrack() {
+		handleSubjects();
+	},[]);
 
 	useEffect(() => {
 		console.log(currentSubjects);
--- frontend/src/graphql/queries.ts
+++ frontend/src/graphql/queries.ts
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 import { TypedDocumentNode } from '@apollo/client';
-import { Subject } from '@server/types.ts';
+import { Assignment, Subject } from '@server/types.ts';
 
-export const CURRENT_SUBJECTS_QUERY: TypedDocumentNode<Subject> = gql`
+export const CURRENT_SUBJECTS_QUERY: TypedDocumentNode<{subjects: Subject[]}> = gql`
     query CurrentSubjectsQuery {
         getCurrentSubjects {
             unitCode,
